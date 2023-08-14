2e3e1598fbc55bb2394593f5f4274384dcfd9af8
Jesus Zuñiga <jesuszt@hotmail.com>
2023-08-14 15:52:18 -0600

    Se agrega método crash

--- mcsv-usuarios/src/main/java/org/zutjmx/spingcloud/mcsv/usuarios/controllers/UsuarioController.java
+++ mcsv-usuarios/src/main/java/org/zutjmx/spingcloud/mcsv/usuarios/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package org.zutjmx.spingcloud.mcsv.usuarios.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -16,6 +18,14 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private ApplicationContext context;
+
+    @GetMapping("/crash")
+    public void crash() {
+        ((ConfigurableApplicationContext)context).close();
+    }
 
     @GetMapping("/")
     public Map<String,List<Usuario>> listar() {
