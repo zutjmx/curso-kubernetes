d78142f67be28c05341dcaf8f5694cca0022fb65
Jesus Zuñiga <jesuszt@hotmail.com>
2023-08-18 14:46:08 -0600

    Agregando ambientes

--- mcsv-usuarios/src/main/java/org/zutjmx/spingcloud/mcsv/usuarios/controllers/UsuarioController.java
+++ mcsv-usuarios/src/main/java/org/zutjmx/spingcloud/mcsv/usuarios/controllers/UsuarioController.java
@@ -3,6 +3,7 @@ package org.zutjmx.spingcloud.mcsv.usuarios.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -22,14 +23,21 @@ public class UsuarioController {
     @Autowired
     private ApplicationContext context;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/crash")
     public void crash() {
         ((ConfigurableApplicationContext)context).close();
     }
 
     @GetMapping("/")
-    public Map<String,List<Usuario>> listar() {
-        return Collections.singletonMap("users",usuarioService.Listar());
+    public ResponseEntity<?> listar() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("users",usuarioService.Listar());
+        body.put("pod_info",environment.getProperty("MY_POD_NAME") + ":" + environment.getProperty("MY_POD_IP"));
+        body.put("texto", environment.getProperty("config.texto"));
+        return ResponseEntity.ok(body);
     }
 
     @GetMapping("/{id}")
